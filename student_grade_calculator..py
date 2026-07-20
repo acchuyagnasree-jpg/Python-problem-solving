2c870c94dd4f7cca7612756257ae251162aaf6d9
ACCHU YAGNA SREE <acchuyagnasree@gmail.com>
2026-07-20 21:17:16 +0530

    Create student_grade_calculator..py

---
+++ student_grade_calculator..py
@@ -0,0 +1,17 @@
+Create a class Student.
+
+Attributes:
+- name
+- marks (list of 5 subjects)
+
+Methods:
+- calculate_average()
+- find_grade()
+- display_result()
+
+Grade Rules:
+90+  -> A
+80-89 -> B
+70-79 -> C
+60-69 -> D
+Below 60 -> F
